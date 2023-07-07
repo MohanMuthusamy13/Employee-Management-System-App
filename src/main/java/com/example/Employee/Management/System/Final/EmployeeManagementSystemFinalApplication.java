286eb87c04ab19625f495aba8b4544498762c995
Mohanraj <mohanmoji1313@gmail.com>
2023-07-07 11:27:25 +0530

    init commit

---
+++ src/main/java/com/example/Employee/Management/System/Final/EmployeeManagementSystemFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.Employee.Management.System.Final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSystemFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementSystemFinalApplication.class, args);
+	}
+
+}
